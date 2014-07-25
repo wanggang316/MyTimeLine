b272d2576f4caa9c6a491cf356aeab48a519d7cb
wanggang <wgang316@163.com>
2014-07-25 22:01:53 +0800

    Initial Commit

---
+++ ObjcTimeLine/WGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WGAppDelegate.h
+//  ObjcTimeLine
+//
+//  Created by 王刚 on 14/7/25.
+//  Copyright (c) 2014年 wwwlife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ObjcTimeLine/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ObjcTimeLine
+//
+//  Created by 王刚 on 14/7/25.
+//  Copyright (c) 2014年 wwwlife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WGAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WGAppDelegate class]));
+    }
+}
